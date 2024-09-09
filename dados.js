d7ec9500922049bb26ca5c9b3fae863e2b77ff35
marciocastro32 <92818437+marciocastro32@users.noreply.github.com>
2024-09-09 11:35:48 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Popularmente chamada de “Fadinha do Skate”, Rayssa ganhou esse apelido após seu vídeo fazendo manobras de skate fantasiada de fada viralizar na internet aos sete anos de idade.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    
+    },
+    {
+        titulo: "Rebeca Andade",
+        descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com seu talento e determinação. Bicampeã olímpica, ela é considerada a maior medalhista olímpica da história do Brasil. Sua trajetória é marcada por superação e dedicação, inspirando jovens atletas em todo o país.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "judô judo"
+    
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou o coração dos brasileiros com sua determinação e técnica. Ela é considerada uma das maiores promessas do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "ginastica ginástica ouro"
+    
+    }
+       
+
+];
+
